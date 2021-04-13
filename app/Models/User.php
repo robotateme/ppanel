04908ac1dd5d4bfdb9a3dcd568804b9428cbc67f
Ярослав Леженко <yaroslav.lezhenko@i3donline.com>
2021-04-14 00:06:56 +0300

    git file mode

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, 'user_books');
+    }
 }
